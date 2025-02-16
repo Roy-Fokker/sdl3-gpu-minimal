1f278332b84778285c5d91d259ec4bb4e34d13b2
Roy-Fokker <11281115+Roy-Fokker@users.noreply.github.com>
2025-02-16 14:35:08 -0500

    push depth on frag

--- shaders/grid.fs.hlsl
+++ shaders/grid.fs.hlsl
@@ -5,6 +5,20 @@ struct Input
 	float4 FarPoint : TEXCOORD1;
 };
 
+struct Output
+{
+	float4 color : SV_Target;
+	float depth : SV_Depth;
+};
+
+struct FrameBuffer
+{
+	float4x4 projection;
+	float4x4 view;
+};
+
+ConstantBuffer<FrameBuffer> ubo : register(b0, space3);
+
 float4 grid(float3 pos, float scale)
 {
 	float2 coord = pos.xz * scale;
@@ -30,13 +44,24 @@ float4 grid(float3 pos, float scale)
 	return color;
 }
 
-float4 main(Input input) : SV_Target0
+float compute_depth(float3 pos)
+{
+	float4 clip_space_pos = mul(ubo.projection, mul(ubo.view, float4(pos.xyz, 1.f)));
+	return clip_space_pos.z / clip_space_pos.w;
+}
+
+Output main(Input input)
 {
 	float t = -input.NearPoint.y / (input.FarPoint.y - input.NearPoint.y);
 
 	float3 pos = input.NearPoint.xyz + t * (input.FarPoint.xyz - input.NearPoint.xyz);
 
 	float4 outColor = grid(pos, 10) * float(t > 0);
+	float depth = compute_depth(pos);
 
-	return outColor;
+	Output output;
+	output.color = outColor;
+	output.depth = depth;
+
+	return output;
 }
